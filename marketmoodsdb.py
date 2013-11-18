3246478e9df3023a77832cfac157c17a330169d5
Pinwheeler <anthonydreessen@gmail.com>
2013-11-17 19:03:39 -0500

    phase 3 complete

--- marketmoodsdb.py
+++ marketmoodsdb.py
@@ -12,7 +12,7 @@ from nltk import FreqDist
 from xgoogle.search import GoogleSearch, SearchError
 
 #Connect to Database
-db = pymysql.connect(host="localhost", user="root", passwd="", db="market_moods_db")
+db = pymysql.connect(host="localhost", user="root", passwd="", db="market_moods_db_backup")
 cur = db.cursor()
 
 #querey database for all company names
@@ -22,6 +22,7 @@ companies = cur.fetchall()
 for row in companies :
 	#get the stock change and price
 	stock_name = row[0]
+	stock_ticker = row[1]
 	stock_change = ystockquote.get_change(row[1])
 	stock_price = float(ystockquote.get_price(row[1]))
 
@@ -35,10 +36,6 @@ for row in companies :
 		continue_check = False
 
 	if continue_check:
-		#create a new movements object with this information
-		#
-		#
-		#
 
 		new_stock_change = 0
 		# convert the stock change into a number
@@ -69,8 +66,12 @@ for row in companies :
 		stopwords = nltk.corpus.stopwords.words('english')
 		words = sorted([w for w in set(text) if len(w) >= min_word_length and fdist[w] >= min_word_frequency and w.lower() not in stopwords])
 
+		overall_company_valence = 0
+		company_word_count = 0
+
 		for word in words:
 			word = word.lower()
+			company_word_count = company_word_count + 1
 			valence = 0
 			count = 1
 			execute_string = 'SELECT * FROM valences WHERE word = \"' + word + '\"'
@@ -81,16 +82,47 @@ for row in companies :
 			else:
 				valence = wordrows[0][1] + stock_change
 				count = wordrows[0][2] + 1
-			average_valence = valence / count	
+			average_valence = valence / count
+			overall_company_valence = overall_company_valence + average_valence
+
+			#update global variances	
 			args = (word,valence,count,average_valence)
-			cur.callproc("updatevalence",args)
+			#cur.callproc("updatevalence",args)
+
+		#create prediction for tommorow
+		overall_company_valence = overall_company_valence / company_word_count
+		create_args = (stock_ticker, str(overall_company_valence), stock_name)
+		create_string = str(create_args)
+		cur.execute("CALL create_prediction" + create_string)
+
+		#update movement table object with this information
+		fetchargs = (stock_ticker,1,1,1)
+		fetch_string = "CALL fetch_movement_today" + str(fetchargs)
+		rows = cur.execute(fetch_string)
+		movement = cur.fetchall()
+		predicted_move = movement[0][2]
+		difference = 0
+		try:
+			difference = stock_change - predicted_move
+		except Exception, e:
+			predicted_move = 1;
+		if stock_change == 0:
+			stock_change = 1
+		percentage = min(stock_change,predicted_move)/max(stock_change,predicted_move)
+		movement_update_args = (stock_ticker,stock_change,difference,percentage,stock_name)
+		
+		update_string = "CALL evaluate_prediction" + str(movement_update_args)
+		cur.execute(update_string)
+
 	else:
 		print stock_name + ": No Change in Stock Price"
 
 #commit and close connection
+#disable this when testing the script
 db.commit()
-print "Valences committed"
 
 cur.close()
 db.close()
+print "MarketMoods run complete"
+print('\a')
 
---
+++ marketmoodspred.py
@@ -0,0 +1,1 @@
+#marketmoodspred.py
